0b09fc7cbf0f5d6ee7b6103d1d6b810d2d645839
Robert Farb <farb.robert@gmail.com>
2018-08-07 16:23:55 -0700

    Annotate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  zip_code        :integer          not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :username, :first_name, :last_name, :email, :password_digest, presence: true
   validates :username, :session_token, uniqueness: true
@@ -6,9 +22,7 @@ class User < ApplicationRecord
 
   after_initialize :ensure_session_token
 
-  # has_many :bookings,
-  #   through: :listings
-
+  
   def self.generate_session_token
     SecureRandom.urlsafe_base64
   end
