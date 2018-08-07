0fa95ca76619060f812e635fbd8033d0ed9e5529
Robert Farb <farb.robert@gmail.com>
2018-08-07 13:10:32 -0700

    Add create to users controller and complete users show jbuilder

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def create
-    
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :first_name, 
+    :last_name, :email, :zip_code)
   end
 end
