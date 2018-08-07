12e05ad8436c9e46b6898166a4eff1d3276f5044
Robert Farb <farb.robert@gmail.com>
2018-08-07 11:47:42 -0700

    Add static page controller and root to static pages root

---
+++ frontend/campout.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Root from './components.root';
+// import configureStore from './store/store';
+
+// document.addEventListener("DOMContentLodaed", () => {
+//   const root = document.getElementById("root");
+//   let store;
+
+//   store = configureStore(preloadedState());
+//   ReactDOM.render(<h1>Hello World</h1>, root)
+// });
