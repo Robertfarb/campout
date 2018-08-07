e9013514a67f30ea52163165adbc94d105aea014
Robert Farb <farb.robert@gmail.com>
2018-08-07 16:23:11 -0700

    add signup login and logout thunk actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -5,20 +5,37 @@ export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 
 
-
-
-
 export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
   currentUser
 });
 
-export const logoutCurrentUser = currentUser => ({
+export const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER,
-  currentUser
 });
 
-export const RECEOVE_SESSION_ERRORS = currentUser => ({
+export const receiveErrors = errors => ({
   type: RECEIVE_SESSION_ERRORS,
-  currentUser
+  errors
 });
+
+export const signup = user => dispatch => (
+  SessAPIUtil.signup(user)
+  .then(user => (dispatch(receiveCurrentUser(user))
+  ), err => (dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const login = user => dispatch => (
+  SessAPIUtil.login(user)
+  .then(user => (dispatch(receiveCurrentUser(user))
+  ), err => (dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logout  = user => dispatch => (
+  SessAPIUtil.logout(user)
+  .then(user => (dispatch(logoutCurrentUser(user))
+  ), err => (dispatch(receiveErrors(err.responseJSON))
+  ))
+);
