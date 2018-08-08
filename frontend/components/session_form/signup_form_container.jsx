084555b21f79f56fe927a7ddce5895d4f226b59e
Robert Farb <farb.robert@gmail.com>
2018-08-07 21:17:09 -0700

    create login_form_container

---
+++ frontend/components/session_form/signup_form_container.jsx
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { signup } from '../../actions/session_actions'
+import { openModal, closeModal } from '../../modal_actions'
+import SessionForm from './session_form';
+
+const mapStateToProps = ({ errors }) => ({
+  errors: errors.session,
+  formType: 'signup'
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    processForm: (user) => dispatch(signup(user)),
+    otherForm: (
+      <button onClick={() => dispatch(openModal('login'))}>Login</button>
+    ),
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
