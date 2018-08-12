da497738cec18c7a6651d97d491c2761401b7027
Robert Farb <farb.robert@gmail.com>
2018-08-11 18:38:54 -0700

    Add styling to splash and seed campsite images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users.delete_all
+listings.delete_all
+
 User.create({username: "robfarb", first_name: "Robert", last_name: "Farb", zip_code: "94103", password: "12345678"})
 User.create({username: "smokeythebear", first_name: "Smokey", last_name: "Bear", zip_code: "94103", password: "12345678"})
 User.create({username: "parkranger10", first_name: "Ranger", last_name: "Rick", zip_code: "92234", password: "ilovenationalparks"})
+User.create({username: "nationalparkservice", first_name: "National", last_name: "Park Service", zip_code: "92234", password: "natparkservice10"})
 
 yos = Listing.create!({title: "Yosemite", host_id: 1, description: "Yosemite National Park lies in the heart of California. It flaunts 'hanging' valleys, many waterfalls, cirque lakes, polished domes, moraines and U-shaped valleys.",
                 address: "Yosemite National Park", long: -119.538329, lat: 37.865101, price_daily: 55, is_glamping: false, 
-                pet_friendly: false, is_toilets: false, max_capacity: 5, has_showers: false, checkin_after: "10:00 am", 
-                checkout_before: "4:00 pm"})
+                pet_friendly: false, is_toilets: false, max_capacity: 5, has_showers: false, checkin_after: "10 AM", 
+                checkout_before: "4 PM"})
                 
                 # yos.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/yosemite/yosemite_1.jpg"), filename: "yosemite-1")
                 # yos.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/yosemite/yosemite_2.jpg"), filename: "yosemite-2")
@@ -23,8 +27,8 @@ yos = Listing.create!({title: "Yosemite", host_id: 1, description: "Yosemite Nat
                 
 big_sur = Listing.create!({title: "Big Sur", host_id: 3, description: "Experience one of the world's most unforgettable stretches of coastline in and around Big Sur, California.", 
                 address: "Big Sur National Park", long: -121.856261, lat: 36.361475, price_daily: 120, is_glamping: false, 
-                pet_friendly: true, is_toilets: true, max_capacity: 6, has_showers: true, checkin_after: "11:00 am", 
-                checkout_before: "3:00 pm"})
+                pet_friendly: true, is_toilets: true, max_capacity: 6, has_showers: true, checkin_after: "11 AM", 
+                checkout_before: "3 PM"})
 
                 # big_sur.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/big_sur/big_sur1.jpg"), filename: "big_sur-1")
                 # big_sur.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/big_sur/big_sur2.jpg"), filename: "big_sur-2")
@@ -35,8 +39,8 @@ big_sur = Listing.create!({title: "Big Sur", host_id: 3, description: "Experienc
 
 j_tree = Listing.create!({title: "Joshua Tree", host_id: 3, description: "Arid low desert and vegetated high desert meet in Joshua Tree National Park, offering visitors stunning vistas and rewarding hikes, and incredible climbs.", 
                 address: "Joshua Tree National Park", long: -115.900993, lat: 33.873417, price_daily: 60, is_glamping: false, 
-                pet_friendly: false, is_toilets: true, max_capacity: 10, has_showers: false, checkin_after: "10:00 am", 
-                checkout_before: "4:00 pm"})
+                pet_friendly: false, is_toilets: true, max_capacity: 10, has_showers: false, checkin_after: "10 AM", 
+                checkout_before: "4 PM"})
 
                 # j_tree.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/joshua_tree/joshua_tree1.jpg"), filename: "jtree1")
                 # j_tree.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/joshua_tree/joshua_tree2.jpg"), filename: "jtree2")
@@ -47,14 +51,20 @@ j_tree = Listing.create!({title: "Joshua Tree", host_id: 3, description: "Arid l
 
 zion = Listing.create!({title: "Zion", host_id: 1, description: "Zion is a beautiful, vast nature reserve in Southwest Utah offering hikes through slot canyons and endless trails through renowned red canyons", 
                 address: "Zion National Park", long: -113.026299, lat: 37.298203, price_daily: 105, is_glamping: false, 
-                pet_friendly: true, is_toilets: false, max_capacity: 10, has_showers: false, checkin_after: "10:00 am", 
-                checkout_before: "4:00 pm"})
+                pet_friendly: true, is_toilets: false, max_capacity: 10, has_showers: false, checkin_after: "10 AM", 
+                checkout_before: "4 PM"})
 
                 # zion.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/zion/zion1.jpg"), filename: "zion1")
                 # zion.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/zion/zion2.jpg"), filename: "zion3")
                 # zion.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/zion/zion3.jpg"), filename: "zion4")
                 # zion.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/zion/zion4.jpg"), filename: "zion5")
                 # zion.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/zion/zion5.jpg"), filename: "zion6")
+
+                
+crater_lake = Listing.create!({title: "Crater Lake", host_id: 4, description: "Explore the beatuty that is Crater Lake National Park in the Cascade Mountains of southern Oregon. It’s known for its namesake Crater Lake, formed by the now-collapsed volcano, Mount Mazama.", 
+                address: "Crater Lake National Park", long: 122.1685, lat: 42.8684, price_daily: 100, is_glamping: false, 
+                pet_friendly: false, is_toilets: true, max_capacity: 8, has_showers: false, checkin_after: "10 AM", 
+                checkout_before: "4 PM"})
                
 
 
--- frontend/components/home/main.jsx
+++ frontend/components/home/main.jsx
@@ -13,23 +13,89 @@ class MainPage extends React.Component {
   render() {
     return (
       <div className="main-container">
-        <div className="left-main">
-          <h2>Campout makes camping easy.</h2><br/>
-          <div className="tagline">
-            <p>Book unique camping experiences at 300,000 <br /> campsites, ranches, vineyards, public parks and more.</p>
+        <div className="main-top">
+          <div className="left-main">
+            <h2>Campout makes camping easy.</h2><br/>
+            <div className="tagline">
+              <p>Book unique camping experiences at 300,000 <br /> campsites, ranches, vineyards, public parks and more.</p>
+            </div>
+            <div className="search-bar-container">
+              <input placeholder="Camping near me..." className="campsite-search" type="text"/>
+            </div>
           </div>
-          <div className="search-bar-container">
-            <input placeholder="Camping near me..." className="campsite-search" type="text"/>
-          </div>
-        </div>
-        <div className="right-main">
-          <div className="right-img-conatiner">
-            <img src={window.images.yosemiteMain} alt="Yosemite National Park"/>
-            <p><Link to="/">Yosemite Nat. Park</Link> hosted by National Park Service</p>
+          <div className="right-main">
+            <div className="right-img-conatiner">
+              <img src={window.images.yosemiteMain} alt="Yosemite National Park"/>
+              <p><Link to="/">Yosemite Nat. Park</Link> hosted by National Park Service</p>
+            </div>
           </div>
         </div>
         <div className="splash-listing-images">
-
+          <h2>Discover camping...</h2>
+          <div className="splash-square-grid">
+            <div className="splash-square">
+              <div className="splash-listing-img"><img className="full-img" src="https://s3-us-west-1.amazonaws.com/campout-dev/puppy-1234691_1920.jpg" alt="Pet friendly Camping"/></div>
+              <footer className="splash-image-footer">
+                <h3>Pet friendly camping</h3>
+                <p>Best options near me</p>
+              </footer>
+            </div>
+            <div className="splash-square">
+              <div className="splash-listing-img"><img className="full-img" src="https://s3-us-west-1.amazonaws.com/campout-dev/glapming.jpg" alt="Glamping" /></div>
+              <footer className="splash-image-footer">
+                <h3>Glamping near me</h3>
+                <p>Best options near me</p>
+              </footer>
+            </div>
+            <div className="splash-square">
+              <div className="splash-listing-img"><img className="full-img" src="https://s3-us-west-1.amazonaws.com/campout-dev/martine-jacobsen-344052-unsplash.jpg" alt="" /></div>
+              <footer className="splash-image-footer">
+                <h3>Beach camping</h3>
+                <p>Best options near me</p>
+              </footer>
+            </div>
+            <div className="splash-square">
+              <div className="splash-listing-img"><img className="full-img" src="https://s3-us-west-1.amazonaws.com/campout-dev/vlad-shapochnikov-127780-unsplash.jpg" alt="" /></div>
+              <footer className="splash-image-footer">
+                <h3>Crater Lake</h3>
+                <p>Oregon</p>
+              </footer>
+            </div>
+            <div className="splash-square">
+              <div className="splash-listing-img"><img className="full-img" src="https://s3-us-west-1.amazonaws.com/campout-dev/big_sur.jpg" alt="" /></div>
+              <footer className="splash-image-footer">
+              <h3>Big Sur</h3>
+              <p>California</p>
+              </footer>
+            </div>
+            <div className="splash-square">
+              <div className="splash-listing-img"><img className="full-img" src="https://s3-us-west-1.amazonaws.com/campout-dev/zion.jpg" alt="" /></div>
+              <footer className="splash-image-footer">
+                <h3>Zion</h3>
+                </p>Utah</p>
+              </footer>
+            </div>
+            <div className="splash-square">
+              <div className="splash-listing-img"><img className="full-img" src="https://s3-us-west-1.amazonaws.com/campout-dev/joshua_tree.jpg" alt="" /></div>
+              <footer className="splash-image-footer">
+                <h3>Joshua Tree</h3>
+                <p>California</p>
+              </footer>
+            </div>
+            <div className="splash-square">
+              <div className="splash-listing-img"><img className="full-img" src="https://s3-us-west-1.amazonaws.com/campout-dev/grand_canyon.jpg" alt="" /></div>
+              <footer className="splash-image-footer">
+                <h3>Grand Canyon</h3>
+                <p>Arizona</p>
+              </footer>
+            </div>
+            <div className="splash-square">
+              <div className="splash-listing-img"><img className="full-img" src="https://s3-us-west-1.amazonaws.com/campout-dev/redwood.jpg" alt="" /></div>
+              <footer className="splash-image-footer">
+                <h3>Redwoods</h3>
+              </footer>
+            </div>
+          </div>
         </div>
       </div>
     );
