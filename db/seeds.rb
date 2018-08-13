4e8e080c8c2cfb75c30274b93c6127e38c8af8c7
Robert Farb <farb.robert@gmail.com>
2018-08-12 21:27:13 -0700

    Seed core 6 campsites

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ User.delete_all
 Listing.delete_all
 Activity.delete_all
 
-User.create({username: "robfarb", first_name: "Robert", last_name: "Farb", zip_code: "94103", password: "12345678"})
-User.create({username: "smokeythebear", first_name: "Smokey", last_name: "Bear", zip_code: "94103", password: "12345678"})
-User.create({username: "parkranger10", first_name: "Ranger", last_name: "Rick", zip_code: "92234", password: "ilovenationalparks"})
-User.create({username: "nationalparkservice", first_name: "National", last_name: "Park Service", zip_code: "92234", password: "natparkservice10"})
+rob = User.create({username: "robfarb", first_name: "Robert", last_name: "Farb", zip_code: "94103", password: "12345678"})
+smokey = User.create({username: "smokeythebear", first_name: "Smokey", last_name: "Bear", zip_code: "94103", password: "12345678"})
+ranger_rick = User.create({username: "parkranger10", first_name: "Ranger", last_name: "Rick", zip_code: "92234", password: "ilovenationalparks"})
+national_park_service = User.create({username: "nationalparkservice", first_name: "National", last_name: "Park Service", zip_code: "92234", password: "natparkservice10"})
 
-yos = Listing.create!({title: "Yosemite", host_id: 1, description: "Yosemite National Park lies in the heart of California. It flaunts 'hanging' valleys, many waterfalls, cirque lakes, polished domes, moraines and U-shaped valleys.",
+yos = Listing.create!({title: "Yosemite", host_id: 4, description: "Yosemite National Park lies in the heart of California. It flaunts 'hanging' valleys, many waterfalls, cirque lakes, polished domes, moraines and U-shaped valleys.",
                 address: "Yosemite National Park", long: -119.538329, lat: 37.865101, price_daily: 55, is_glamping: false, 
                 pet_friendly: false, is_toilets: false, max_capacity: 5, has_showers: false, checkin_after: "10 AM", 
                 checkout_before: "4 PM"})
@@ -26,7 +26,7 @@ yos = Listing.create!({title: "Yosemite", host_id: 1, description: "Yosemite Nat
                 yos.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/yosemite/yosemite_4.jpg"), filename: "yosemite-4")
                 yos.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/yosemite/yosemite_5.jpg"), filename: "yosemite-5")
                 
-big_sur = Listing.create!({title: "Big Sur", host_id: 3, description: "Experience one of the world's most unforgettable stretches of coastline in and around Big Sur, California.", 
+big_sur = Listing.create!({title: "Big Sur", host_id: 4, description: "Experience one of the world's most unforgettable stretches of coastline in and around Big Sur, California.", 
                 address: "Big Sur National Park", long: -121.856261, lat: 36.361475, price_daily: 120, is_glamping: false, 
                 pet_friendly: true, is_toilets: true, max_capacity: 6, has_showers: true, checkin_after: "11 AM", 
                 checkout_before: "3 PM"})
@@ -38,7 +38,7 @@ big_sur = Listing.create!({title: "Big Sur", host_id: 3, description: "Experienc
                 big_sur.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/big_sur/big_sur5.jpg"), filename: "big_sur-5")
                 big_sur.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/big_sur/big_sur6.jpg"), filename: "big_sur-6")
 
-j_tree = Listing.create!({title: "Joshua Tree", host_id: 3, description: "Arid low desert and vegetated high desert meet in Joshua Tree National Park, offering visitors stunning vistas and rewarding hikes, and incredible climbs.", 
+j_tree = Listing.create!({title: "Joshua Tree", host_id: 4, description: "Arid low desert and vegetated high desert meet in Joshua Tree National Park, offering visitors stunning vistas and rewarding hikes, and incredible climbs.", 
                 address: "Joshua Tree National Park", long: -115.900993, lat: 33.873417, price_daily: 60, is_glamping: false, 
                 pet_friendly: false, is_toilets: true, max_capacity: 10, has_showers: false, checkin_after: "10 AM", 
                 checkout_before: "4 PM"})
@@ -50,7 +50,7 @@ j_tree = Listing.create!({title: "Joshua Tree", host_id: 3, description: "Arid l
                 j_tree.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/joshua_tree/joshua_tree5.jpg"), filename: "jtree5")
 
 
-zion = Listing.create!({title: "Zion", host_id: 1, description: "Zion is a beautiful, vast nature reserve in Southwest Utah offering hikes through slot canyons and endless trails through renowned red canyons", 
+zion = Listing.create!({title: "Zion", host_id: 4, description: "Zion is a beautiful, vast nature reserve in Southwest Utah offering hikes through slot canyons and endless trails through renowned red canyons", 
                 address: "Zion National Park", long: -113.026299, lat: 37.298203, price_daily: 105, is_glamping: false, 
                 pet_friendly: true, is_toilets: false, max_capacity: 10, has_showers: false, checkin_after: "10 AM", 
                 checkout_before: "4 PM"})
@@ -67,6 +67,12 @@ crater_lake = Listing.create!({title: "Crater Lake", host_id: 4, description: "E
                 pet_friendly: false, is_toilets: true, max_capacity: 8, has_showers: false, checkin_after: "10 AM", 
                 checkout_before: "4 PM"})
 
+                crater_lake.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/crater_lake/crater_lake1.jpg"), filename: "crater1")
+                crater_lake.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/crater_lake/crater_lake2.jpg"), filename: "crater2")
+                crater_lake.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/crater_lake/crater_lake3.jpg"), filename: "crater3")
+                crater_lake.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/crater_lake/crater_lake4.jpg"), filename: "crater4")
+                crater_lake.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/crater_lake/crater_lake5.jpg"), filename: "crater5")
+
 
 
 grand_canyon = Listing.create!({title: "Grand Canyon", host_id: 4, description: "Come explore the vast canyon carved out by the Colorado River. The Grand Canyon is the most spectacular gorge in the world. Located in the state of Arizona, it cuts across the Grand Canyon National Park.", 
@@ -74,11 +80,24 @@ grand_canyon = Listing.create!({title: "Grand Canyon", host_id: 4, description: 
                 pet_friendly: true, is_toilets: true, max_capacity: 15, has_showers: false, checkin_after: "11 AM", 
                 checkout_before: "4 PM"})
 
+                grand_canyon.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/grand_canyon/grand_canyon1.jpg"), filename: "grand_canyon1")
+                grand_canyon.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/grand_canyon/grand_canyon2.jpg"), filename: "grand_canyon2")
+                grand_canyon.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/grand_canyon/grand_canyon3.jpg"), filename: "grand_canyon3")
+                grand_canyon.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/grand_canyon/grand_canyon4.jpg"), filename: "grand_canyon4")
+                grand_canyon.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/grand_canyon/grand_canyon5.jpg"), filename: "grand_canyon5")
+
 
 redwoods = Listing.create!({title: "Redwoods", host_id: 3, description: "Come experience a true exhibit of scale and perspective when viewing the vast redwoods that this California State Park has to offer.", 
                 address: "Redwoods National Park", long: -124.0046275, lat: 41.2131788, price_daily: 75, is_glamping: false, 
                 pet_friendly: true, is_toilets: true, max_capacity: 8, has_showers: true, checkin_after: "9 AM", 
                 checkout_before: "2 PM"})
+ 
+                redwoods.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/redwood/redwood1.jpg"), filename: "redwood1")
+                redwoods.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/redwood/redwood2.jpg"), filename: "redwood2")
+                redwoods.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/redwood/redwood3.jpg"), filename: "redwood3")
+                redwoods.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/redwood/redwood4.jpg"), filename: "redwood4")
+                redwoods.photos.attach(io: File.open("/Users/robfarb/Desktop/app_academy/campout_seeds/redwood/redwood5.jpg"), filename: "redwood5")
+
                
 
 #Listing Images
