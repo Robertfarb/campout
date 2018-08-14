9a20894bd9fd2347148d8a6f1e17610e06d8b25d
Robert Farb <farb.robert@gmail.com>
2018-08-14 10:57:24 -0700

    fix storage.yml file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "campout_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.active_storage.service =:amazon_dev
+  config.active_storage.service = :amazon_prod
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
