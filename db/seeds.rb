3641caa71e5b888510b5bc54b932b1819dd58353
Robert Farb <farb.robert@gmail.com>
2018-08-14 11:12:44 -0700

    Fix file paths for heroku seed:

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,12 +37,12 @@ big_sur = Listing.create!({title: "Big Sur", host_id: national_park_service.id, 
                 pet_friendly: true, is_toilets: true, max_capacity: 6, has_showers: true, checkin_after: "11 AM", 
                 checkout_before: "3 PM"})
 
-                big_sur.photos.attach(io: File.open("/app/assets/images/big_sur/big_sur1.jpg"), filename: "big_sur-1.jpg")
-                big_sur.photos.attach(io: File.open("/app/assets/images/big_sur/big_sur2.jpg"), filename: "big_sur-2.jpg")
-                big_sur.photos.attach(io: File.open("/app/assets/images/big_sur/big_sur3.jpg"), filename: "big_sur-3.jpg")
-                big_sur.photos.attach(io: File.open("/app/assets/images/big_sur/big_sur4.jpg"), filename: "big_sur-4.jpg")
-                big_sur.photos.attach(io: File.open("/app/assets/images/big_sur/big_sur5.jpg"), filename: "big_sur-5.jpg")
-                big_sur.photos.attach(io: File.open("/app/assets/images/big_sur/big_sur6.jpg"), filename: "big_sur-6.jpg")
+                big_sur.photos.attach(io: File.open("app/assets/images/big_sur/big_sur1.jpg"), filename: "big_sur-1.jpg")
+                big_sur.photos.attach(io: File.open("app/assets/images/big_sur/big_sur2.jpg"), filename: "big_sur-2.jpg")
+                big_sur.photos.attach(io: File.open("app/assets/images/big_sur/big_sur3.jpg"), filename: "big_sur-3.jpg")
+                big_sur.photos.attach(io: File.open("app/assets/images/big_sur/big_sur4.jpg"), filename: "big_sur-4.jpg")
+                big_sur.photos.attach(io: File.open("app/assets/images/big_sur/big_sur5.jpg"), filename: "big_sur-5.jpg")
+                big_sur.photos.attach(io: File.open("app/assets/images/big_sur/big_sur6.jpg"), filename: "big_sur-6.jpg")
 
                 # big_sur.photos.attach(io: File.open("./app/assets/images/big_sur/big_sur1.jpg"), filename: "big_sur-1.jpg")
                 # big_sur.photos.attach(io: File.open("./app/assets/images/big_sur/big_sur2.jpg"), filename: "big_sur-2.jpg")
@@ -56,11 +56,11 @@ j_tree = Listing.create!({title: "Joshua Tree", host_id: ranger_rick.id, descrip
                 pet_friendly: false, is_toilets: true, max_capacity: 10, has_showers: false, checkin_after: "10 AM", 
                 checkout_before: "4 PM"})
 
-                j_tree.photos.attach(io: File.open("/app/assets/images/joshua_tree/joshua_tree1.jpg"), filename: "jtree1.jpg")
-                j_tree.photos.attach(io: File.open("/app/assets/images/joshua_tree/joshua_tree2.jpg"), filename: "jtree2.jpg")
-                j_tree.photos.attach(io: File.open("/app/assets/images/joshua_tree/joshua_tree3.jpg"), filename: "jtree3.jpg")
-                j_tree.photos.attach(io: File.open("/app/assets/images/joshua_tree/joshua_tree4.jpg"), filename: "jtree4.jpg")
-                j_tree.photos.attach(io: File.open("/app/assets/images/joshua_tree/joshua_tree5.jpg"), filename: "jtree5.jpg")
+                j_tree.photos.attach(io: File.open("app/assets/images/joshua_tree/joshua_tree1.jpg"), filename: "jtree1.jpg")
+                j_tree.photos.attach(io: File.open("app/assets/images/joshua_tree/joshua_tree2.jpg"), filename: "jtree2.jpg")
+                j_tree.photos.attach(io: File.open("app/assets/images/joshua_tree/joshua_tree3.jpg"), filename: "jtree3.jpg")
+                j_tree.photos.attach(io: File.open("app/assets/images/joshua_tree/joshua_tree4.jpg"), filename: "jtree4.jpg")
+                j_tree.photos.attach(io: File.open("app/assets/images/joshua_tree/joshua_tree5.jpg"), filename: "jtree5.jpg")
 
                 # j_tree.photos.attach(io: File.open("./app/assets/images/joshua_tree/joshua_tree1.jpg"), filename: "jtree1.jpg")
                 # j_tree.photos.attach(io: File.open("./app/assets/images/joshua_tree/joshua_tree2.jpg"), filename: "jtree2.jpg")
@@ -74,11 +74,11 @@ zion = Listing.create!({title: "Zion", host_id: national_park_service.id, descri
                 pet_friendly: true, is_toilets: false, max_capacity: 10, has_showers: false, checkin_after: "10 AM", 
                 checkout_before: "4 PM"})
 
-                zion.photos.attach(io: File.open("/app/assets/images/zion/zion1.jpg"), filename: "zion1.jpg")
-                zion.photos.attach(io: File.open("/app/assets/images/zion/zion2.jpg"), filename: "zion3.jpg")
-                zion.photos.attach(io: File.open("/app/assets/images/zion/zion3.jpg"), filename: "zion4.jpg")
-                zion.photos.attach(io: File.open("/app/assets/images/zion/zion4.jpg"), filename: "zion5.jpg")
-                zion.photos.attach(io: File.open("/app/assets/images/zion/zion5.jpg"), filename: "zion6.jpg")
+                zion.photos.attach(io: File.open("app/assets/images/zion/zion1.jpg"), filename: "zion1.jpg")
+                zion.photos.attach(io: File.open("app/assets/images/zion/zion2.jpg"), filename: "zion3.jpg")
+                zion.photos.attach(io: File.open("app/assets/images/zion/zion3.jpg"), filename: "zion4.jpg")
+                zion.photos.attach(io: File.open("app/assets/images/zion/zion4.jpg"), filename: "zion5.jpg")
+                zion.photos.attach(io: File.open("app/assets/images/zion/zion5.jpg"), filename: "zion6.jpg")
 
                 # zion.photos.attach(io: File.open("./app/assets/images/zion/zion1.jpg"), filename: "zion1.jpg")
                 # zion.photos.attach(io: File.open("./app/assets/images/zion/zion2.jpg"), filename: "zion3.jpg")
@@ -92,11 +92,11 @@ crater_lake = Listing.create!({title: "Crater Lake", host_id: national_park_serv
                 pet_friendly: false, is_toilets: true, max_capacity: 8, has_showers: false, checkin_after: "10 AM", 
                 checkout_before: "4 PM"})
 
-                crater_lake.photos.attach(io: File.open("/app/assets/images/crater_lake/crater_lake1.jpg"), filename: "crater_lake1.jpg")
-                crater_lake.photos.attach(io: File.open("/app/assets/images/crater_lake/crater_lake2.jpg"), filename: "crater_lake2.jpg")
-                crater_lake.photos.attach(io: File.open("/app/assets/images/crater_lake/crater_lake3.jpg"), filename: "crater_lake3.jpg")
-                crater_lake.photos.attach(io: File.open("/app/assets/images/crater_lake/crater_lake4.jpg"), filename: "crater_lake4.jpg")
-                crater_lake.photos.attach(io: File.open("/app/assets/images/crater_lake/crater_lake5.jpg"), filename: "crater_lake5.jpg")
+                crater_lake.photos.attach(io: File.open("app/assets/images/crater_lake/crater_lake1.jpg"), filename: "crater_lake1.jpg")
+                crater_lake.photos.attach(io: File.open("app/assets/images/crater_lake/crater_lake2.jpg"), filename: "crater_lake2.jpg")
+                crater_lake.photos.attach(io: File.open("app/assets/images/crater_lake/crater_lake3.jpg"), filename: "crater_lake3.jpg")
+                crater_lake.photos.attach(io: File.open("app/assets/images/crater_lake/crater_lake4.jpg"), filename: "crater_lake4.jpg")
+                crater_lake.photos.attach(io: File.open("app/assets/images/crater_lake/crater_lake5.jpg"), filename: "crater_lake5.jpg")
 
                 # crater_lake.photos.attach(io: File.open("./app/assets/images/crater_lake/crater_lake1.jpg"), filename: "crater_lake1.jpg")
                 # crater_lake.photos.attach(io: File.open("./app/assets/images/crater_lake/crater_lake2.jpg"), filename: "crater_lake2.jpg")
@@ -111,11 +111,11 @@ grand_canyon = Listing.create!({title: "Grand Canyon", host_id: national_park_se
                 pet_friendly: true, is_toilets: true, max_capacity: 15, has_showers: false, checkin_after: "11 AM", 
                 checkout_before: "4 PM"})
 
-                grand_canyon.photos.attach(io: File.open("/app/assets/images/grand_canyon/grand_canyon1.jpg"), filename: "grand_canyon1.jpg")
-                grand_canyon.photos.attach(io: File.open("/app/assets/images/grand_canyon/grand_canyon2.jpg"), filename: "grand_canyon2.jpg")
-                grand_canyon.photos.attach(io: File.open("/app/assets/images/grand_canyon/grand_canyon3.jpg"), filename: "grand_canyon3.jpg")
-                grand_canyon.photos.attach(io: File.open("/app/assets/images/grand_canyon/grand_canyon4.jpg"), filename: "grand_canyon4.jpg")
-                grand_canyon.photos.attach(io: File.open("/app/assets/images/grand_canyon/grand_canyon5.jpg"), filename: "grand_canyon5.jpg")
+                grand_canyon.photos.attach(io: File.open("app/assets/images/grand_canyon/grand_canyon1.jpg"), filename: "grand_canyon1.jpg")
+                grand_canyon.photos.attach(io: File.open("app/assets/images/grand_canyon/grand_canyon2.jpg"), filename: "grand_canyon2.jpg")
+                grand_canyon.photos.attach(io: File.open("app/assets/images/grand_canyon/grand_canyon3.jpg"), filename: "grand_canyon3.jpg")
+                grand_canyon.photos.attach(io: File.open("app/assets/images/grand_canyon/grand_canyon4.jpg"), filename: "grand_canyon4.jpg")
+                grand_canyon.photos.attach(io: File.open("app/assets/images/grand_canyon/grand_canyon5.jpg"), filename: "grand_canyon5.jpg")
 
                 # grand_canyon.photos.attach(io: File.open("./app/assets/images/grand_canyon/grand_canyon1.jpg"), filename: "grand_canyon1.jpg")
                 # grand_canyon.photos.attach(io: File.open("./app/assets/images/grand_canyon/grand_canyon2.jpg"), filename: "grand_canyon2.jpg")
@@ -129,11 +129,11 @@ redwoods = Listing.create!({title: "Redwoods", host_id: smokey.id, description: 
                 pet_friendly: true, is_toilets: true, max_capacity: 8, has_showers: true, checkin_after: "9 AM", 
                 checkout_before: "2 PM"})
  
-                redwoods.photos.attach(io: File.open("/app/assets/images/redwood/redwood1.jpg"), filename: "redwood1.jpg")
-                redwoods.photos.attach(io: File.open("/app/assets/images/redwood/redwood2.jpg"), filename: "redwood2.jpg")
-                redwoods.photos.attach(io: File.open("/app/assets/images/redwood/redwood3.jpg"), filename: "redwood3.jpg")
-                redwoods.photos.attach(io: File.open("/app/assets/images/redwood/redwood4.jpg"), filename: "redwood4.jpg")
-                redwoods.photos.attach(io: File.open("/app/assets/images/redwood/redwood5.jpg"), filename: "redwood5.jpg")
+                redwoods.photos.attach(io: File.open("app/assets/images/redwood/redwood1.jpg"), filename: "redwood1.jpg")
+                redwoods.photos.attach(io: File.open("app/assets/images/redwood/redwood2.jpg"), filename: "redwood2.jpg")
+                redwoods.photos.attach(io: File.open("app/assets/images/redwood/redwood3.jpg"), filename: "redwood3.jpg")
+                redwoods.photos.attach(io: File.open("app/assets/images/redwood/redwood4.jpg"), filename: "redwood4.jpg")
+                redwoods.photos.attach(io: File.open("app/assets/images/redwood/redwood5.jpg"), filename: "redwood5.jpg")
 
                 # redwoods.photos.attach(io: File.open("./app/assets/images/redwood/redwood1.jpg"), filename: "redwood1.jpg")
                 # redwoods.photos.attach(io: File.open("./app/assets/images/redwood/redwood2.jpg"), filename: "redwood2.jpg")
