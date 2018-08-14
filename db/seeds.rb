83eab0ab6e5e0c790d060d9dfaca6643cab134db
Robert Farb <farb.robert@gmail.com>
2018-08-14 11:02:38 -0700

    Add local / prod seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,12 @@ yos = Listing.create!({title: "Yosemite", host_id: national_park_service.id, des
                 yos.photos.attach(io: File.open("/app/assets/images/yosemite/yosemite_3.jpg"), filename: "yosemite-3.jpg")
                 yos.photos.attach(io: File.open("/app/assets/images/yosemite/yosemite_4.jpg"), filename: "yosemite-4.jpg")
                 yos.photos.attach(io: File.open("/app/assets/images/yosemite/yosemite_5.jpg"), filename: "yosemite-5.jpg")
+
+                # yos.photos.attach(io: File.open("./app/assets/images/yosemite/yosemite_1.jpg"), filename: "yosemite-1.jpg")
+                # yos.photos.attach(io: File.open("./app/assets/images/yosemite/yosemite_2.jpg"), filename: "yosemite-2.jpg")
+                # yos.photos.attach(io: File.open("./app/assets/images/yosemite/yosemite_3.jpg"), filename: "yosemite-3.jpg")
+                # yos.photos.attach(io: File.open("./app/assets/images/yosemite/yosemite_4.jpg"), filename: "yosemite-4.jpg")
+                # yos.photos.attach(io: File.open("./app/assets/images/yosemite/yosemite_5.jpg"), filename: "yosemite-5.jpg")
                 
 big_sur = Listing.create!({title: "Big Sur", host_id: national_park_service.id, description: "Experience one of the world's most unforgettable stretches of coastline in and around Big Sur, California.", 
                 address: "Big Sur National Park", long: -121.856261, lat: 36.361475, price_daily: 120, is_glamping: false, 
@@ -38,6 +44,13 @@ big_sur = Listing.create!({title: "Big Sur", host_id: national_park_service.id, 
                 big_sur.photos.attach(io: File.open("/app/assets/images/big_sur/big_sur5.jpg"), filename: "big_sur-5.jpg")
                 big_sur.photos.attach(io: File.open("/app/assets/images/big_sur/big_sur6.jpg"), filename: "big_sur-6.jpg")
 
+                # big_sur.photos.attach(io: File.open("./app/assets/images/big_sur/big_sur1.jpg"), filename: "big_sur-1.jpg")
+                # big_sur.photos.attach(io: File.open("./app/assets/images/big_sur/big_sur2.jpg"), filename: "big_sur-2.jpg")
+                # big_sur.photos.attach(io: File.open("./app/assets/images/big_sur/big_sur3.jpg"), filename: "big_sur-3.jpg")
+                # big_sur.photos.attach(io: File.open("./app/assets/images/big_sur/big_sur4.jpg"), filename: "big_sur-4.jpg")
+                # big_sur.photos.attach(io: File.open("./app/assets/images/big_sur/big_sur5.jpg"), filename: "big_sur-5.jpg")
+                # big_sur.photos.attach(io: File.open("./app/assets/images/big_sur/big_sur6.jpg"), filename: "big_sur-6.jpg")
+
 j_tree = Listing.create!({title: "Joshua Tree", host_id: ranger_rick.id, description: "Arid low desert and vegetated high desert meet in Joshua Tree National Park, offering visitors stunning vistas and rewarding hikes, and incredible climbs.", 
                 address: "Joshua Tree National Park", long: -115.900993, lat: 33.873417, price_daily: 60, is_glamping: false, 
                 pet_friendly: false, is_toilets: true, max_capacity: 10, has_showers: false, checkin_after: "10 AM", 
@@ -49,6 +62,12 @@ j_tree = Listing.create!({title: "Joshua Tree", host_id: ranger_rick.id, descrip
                 j_tree.photos.attach(io: File.open("/app/assets/images/joshua_tree/joshua_tree4.jpg"), filename: "jtree4.jpg")
                 j_tree.photos.attach(io: File.open("/app/assets/images/joshua_tree/joshua_tree5.jpg"), filename: "jtree5.jpg")
 
+                # j_tree.photos.attach(io: File.open("./app/assets/images/joshua_tree/joshua_tree1.jpg"), filename: "jtree1.jpg")
+                # j_tree.photos.attach(io: File.open("./app/assets/images/joshua_tree/joshua_tree2.jpg"), filename: "jtree2.jpg")
+                # j_tree.photos.attach(io: File.open("./app/assets/images/joshua_tree/joshua_tree3.jpg"), filename: "jtree3.jpg")
+                # j_tree.photos.attach(io: File.open("./app/assets/images/joshua_tree/joshua_tree4.jpg"), filename: "jtree4.jpg")
+                # j_tree.photos.attach(io: File.open("./app/assets/images/joshua_tree/joshua_tree5.jpg"), filename: "jtree5.jpg")
+
 
 zion = Listing.create!({title: "Zion", host_id: national_park_service.id, description: "Zion is a beautiful, vast nature reserve in Southwest Utah offering hikes through slot canyons and endless trails through renowned red canyons", 
                 address: "Zion National Park", long: -113.026299, lat: 37.298203, price_daily: 105, is_glamping: false, 
@@ -58,8 +77,14 @@ zion = Listing.create!({title: "Zion", host_id: national_park_service.id, descri
                 zion.photos.attach(io: File.open("/app/assets/images/zion/zion1.jpg"), filename: "zion1.jpg")
                 zion.photos.attach(io: File.open("/app/assets/images/zion/zion2.jpg"), filename: "zion3.jpg")
                 zion.photos.attach(io: File.open("/app/assets/images/zion/zion3.jpg"), filename: "zion4.jpg")
-                zion.photos.attach(io: File.open(".app/assets/images/zion/zion4.jpg"), filename: "zion5.jpg")
+                zion.photos.attach(io: File.open("/app/assets/images/zion/zion4.jpg"), filename: "zion5.jpg")
                 zion.photos.attach(io: File.open("/app/assets/images/zion/zion5.jpg"), filename: "zion6.jpg")
+
+                # zion.photos.attach(io: File.open("./app/assets/images/zion/zion1.jpg"), filename: "zion1.jpg")
+                # zion.photos.attach(io: File.open("./app/assets/images/zion/zion2.jpg"), filename: "zion3.jpg")
+                # zion.photos.attach(io: File.open("./app/assets/images/zion/zion3.jpg"), filename: "zion4.jpg")
+                # zion.photos.attach(io: File.open("./app/assets/images/zion/zion4.jpg"), filename: "zion5.jpg")
+                # zion.photos.attach(io: File.open("./app/assets/images/zion/zion5.jpg"), filename: "zion6.jpg")
 
                 
 crater_lake = Listing.create!({title: "Crater Lake", host_id: national_park_service.id, description: "Explore the beatuty that is Crater Lake National Park in the Cascade Mountains of southern Oregon. It’s known for its namesake Crater Lake, formed by the now-collapsed volcano, Mount Mazama.", 
@@ -72,6 +97,12 @@ crater_lake = Listing.create!({title: "Crater Lake", host_id: national_park_serv
                 crater_lake.photos.attach(io: File.open("app/assets/images/crater_lake/crater_lake3.jpg"), filename: "crater_lake3.jpg")
                 crater_lake.photos.attach(io: File.open("app/assets/images/crater_lake/crater_lake4.jpg"), filename: "crater_lake4.jpg")
                 crater_lake.photos.attach(io: File.open("app/assets/images/crater_lake/crater_lake5.jpg"), filename: "crater_lake5.jpg")
+
+                # crater_lake.photos.attach(io: File.open("./app/assets/images/crater_lake/crater_lake1.jpg"), filename: "crater_lake1.jpg")
+                # crater_lake.photos.attach(io: File.open("./app/assets/images/crater_lake/crater_lake2.jpg"), filename: "crater_lake2.jpg")
+                # crater_lake.photos.attach(io: File.open("./app/assets/images/crater_lake/crater_lake3.jpg"), filename: "crater_lake3.jpg")
+                # crater_lake.photos.attach(io: File.open("./app/assets/images/crater_lake/crater_lake4.jpg"), filename: "crater_lake4.jpg")
+                # crater_lake.photos.attach(io: File.open("./app/assets/images/crater_lake/crater_lake5.jpg"), filename: "crater_lake5.jpg")
 
 
 
@@ -86,6 +117,12 @@ grand_canyon = Listing.create!({title: "Grand Canyon", host_id: national_park_se
                 grand_canyon.photos.attach(io: File.open("/app/assets/images/grand_canyon/grand_canyon4.jpg"), filename: "grand_canyon4.jpg")
                 grand_canyon.photos.attach(io: File.open("/app/assets/images/grand_canyon/grand_canyon5.jpg"), filename: "grand_canyon5.jpg")
 
+                # grand_canyon.photos.attach(io: File.open("./app/assets/images/grand_canyon/grand_canyon1.jpg"), filename: "grand_canyon1.jpg")
+                # grand_canyon.photos.attach(io: File.open("./app/assets/images/grand_canyon/grand_canyon2.jpg"), filename: "grand_canyon2.jpg")
+                # grand_canyon.photos.attach(io: File.open("./app/assets/images/grand_canyon/grand_canyon3.jpg"), filename: "grand_canyon3.jpg")
+                # grand_canyon.photos.attach(io: File.open("./app/assets/images/grand_canyon/grand_canyon4.jpg"), filename: "grand_canyon4.jpg")
+                # grand_canyon.photos.attach(io: File.open("./app/assets/images/grand_canyon/grand_canyon5.jpg"), filename: "grand_canyon5.jpg")
+
 
 redwoods = Listing.create!({title: "Redwoods", host_id: smokey.id, description: "Come experience a true exhibit of scale and perspective when viewing the vast redwoods that this California State Park has to offer.", 
                 address: "Redwoods National Park", long: -124.0046275, lat: 41.2131788, price_daily: 75, is_glamping: false, 
@@ -97,6 +134,12 @@ redwoods = Listing.create!({title: "Redwoods", host_id: smokey.id, description: 
                 redwoods.photos.attach(io: File.open("/app/assets/images/redwood/redwood3.jpg"), filename: "redwood3.jpg")
                 redwoods.photos.attach(io: File.open("/app/assets/images/redwood/redwood4.jpg"), filename: "redwood4.jpg")
                 redwoods.photos.attach(io: File.open("/app/assets/images/redwood/redwood5.jpg"), filename: "redwood5.jpg")
+
+                # redwoods.photos.attach(io: File.open("./app/assets/images/redwood/redwood1.jpg"), filename: "redwood1.jpg")
+                # redwoods.photos.attach(io: File.open("./app/assets/images/redwood/redwood2.jpg"), filename: "redwood2.jpg")
+                # redwoods.photos.attach(io: File.open("./app/assets/images/redwood/redwood3.jpg"), filename: "redwood3.jpg")
+                # redwoods.photos.attach(io: File.open("./app/assets/images/redwood/redwood4.jpg"), filename: "redwood4.jpg")
+                # redwoods.photos.attach(io: File.open("./app/assets/images/redwood/redwood5.jpg"), filename: "redwood5.jpg")
 
                
 
