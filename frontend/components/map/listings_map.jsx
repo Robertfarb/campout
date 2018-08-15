67ed963ef3b323e09cb6f12536380f29f88d119a
Robert Farb <farb.robert@gmail.com>
2018-08-14 20:36:30 -0700

    Render map

--- frontend/components/map/listings_map.jsx
+++ frontend/components/map/listings_map.jsx
@@ -6,10 +6,19 @@ class ListingsMap extends React.Component {
     super(props);
   }
 
+  componentDidMount () {
+    const mapOptions = {
+      center: {lat: 37.7758, lng: -122.435},
+      zoom: 13
+    };
+
+    this.map = new google.maps.Map(this.mapNode, mapOptions)
+  }
+
   render() {
     return (
       <div className="listings-map-wrapper">
-        <div id='map-container' ref='map' className='listings-map'>
+        <div id='map-container' ref={map => this.mapNode = map} className='listings-map'>
           
         </div>
       </div>
